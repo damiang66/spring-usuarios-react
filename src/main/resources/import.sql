44adb64d2d52ec4e192ba6c6f21d34fa4ff25800
damian <adamodamian6@gmail.com>
2023-05-01 11:15:44 -0300

    agregando insert.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO usuarios (username,password,email) VALUES('Damian', '12345', 'damian@gmail.com');
+INSERT INTO usuarios (username,password,email) VALUES('Camila', '12345', 'camila@gmail.com');
+INSERT INTO usuarios (username,password,email) VALUES('Josefina', '12345', 'josefina@gmail.com');
+INSERT INTO usuarios (username,password,email) VALUES('Fran', '12345', 'fran@gmail.com');
